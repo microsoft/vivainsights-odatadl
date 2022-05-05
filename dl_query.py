eb613276a3a046f43309a752cdb278d90f2ebb03
Martin Chan <martinchan53@gmail.com>
2022-05-05 13:56:42 +0100

    fix: 10k row limit error (#4)
    Implement fix as per #4 - code contributions from @edama87 and @simrang5395

--- dl_query.py
+++ dl_query.py
@@ -8,12 +8,16 @@ import requests
 import msal
 from requests.structures import CaseInsensitiveDict
 
-# Enter parameters here
+# Enter parameters here ---------------------------------------------------------------------------------------------
 par_authority = 'https://login.microsoftonline.com/<your-tenant>/'
 par_client_cred = '<secret>'
 par_client_id = '<client-id-or-app-id>'
 par_odata = '<odata-link>'
-par_outfile = 'query_data_from_py.csv'
+
+par_outfile_prefix = 'query_data_from_py_' # Prefix for output file
+par_outfile_ext = '.csv' # Extension for output file
+
+# Main script starts -------------------------------------------------------------------------------------------------
 
 app = msal.ConfidentialClientApplication(
     client_id = par_client_id,
@@ -28,11 +32,15 @@ if "access_token" in result:
     headers["Accept"] = "application/json"
     headers["Authorization"] = "Bearer "+result['access_token']    
     print(result['access_token'])
-    r = requests.get(  
-        url = par_odata,
-        headers=headers )
     
-    print("Status code: ", r.status_code)
+    file_counter = 0
+    while par_odata != None: 
+        r = requests.get(  
+            url = par_odata,
+            headers = headers
+        )
+    
+        print("Status code: ", r.status_code)
 
     ## TOGGLE FOR DEBUGGING ONLY
     # file = open("resp_text.txt", "w", encoding = "utf-8")
@@ -40,6 +48,11 @@ if "access_token" in result:
 
     data = json.loads(r.text)
     person_data = data['value']
+    
+    # Temporary file name based on prefix and extension parameters
+    tmp_filename = par_outfile_prefix + str(file_counter).zfill(4) + par_outfile_ext
+    data_file = open(tmp_filename, 'w', newline = '')
+    
     data_file = open(par_outfile, 'w', newline = '')
     csv_writer = csv.writer(data_file)
     count = 0
@@ -57,6 +70,13 @@ if "access_token" in result:
     data_file.close()
 
     print("Query data has been saved to file.")
+    
+    if data['@odata.nextLink']:
+        print("More data exists")
+        par_odata=data['@odata.nextLink']
+        file_counter += 1
+    else:
+        par_odata = None
 
 else:
     print(result.get("error"))
